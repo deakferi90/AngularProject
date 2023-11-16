c4dd626383971d4f8a09b49db4943bebe47f34c4
Ferenc Deak <ferenc.deak@accesa.eu>
2023-11-16 16:03:50 +0200

    added courses component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
+  //challenge
+  //step 1: display courses using ngfor
+  //step 2: add event handler to select home
+  //step 3: display raw json of selected course
+  displayRawJSON: any = '';
+  courses = [
+    {
+      id: 1,
+      title: 'Hello Angular',
+      description: 'Learn the fundamentals of Angular',
+      percent: 26,
+      favorite: true
+    },
+    {
+      id: 1,
+      title: 'Javascript the hard parts',
+      description: 'Learn the hard parts of Javascript',
+      percent: 50,
+      favorite: true
+    },
+    {
+      id: 1,
+      title: 'Feri is learning Angular',
+      description: 'But first learn the hard parts of Javascript',
+      percent: 75,
+      favorite: true
+    }
+  ]
+
+  updateBox(course: any) {
+    this.displayRawJSON = course;
+  }
 }
