045001813197794f3ca13719217aa4bd53cfecca
Ferenc Deak <ferenc.deak@accesa.eu>
2024-01-03 10:45:01 +0200

    added async pipe changes but commented the code

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,18 +1,15 @@
-import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { BooksService } from '../shared/services/books.service';
 import { Book } from '../shared/interfaces/books.interface';
-//import { Observable } from 'rxjs';
-//import { removeItem } from './books.actions';
-import { Store } from '@ngrx/store';
-
+import { Observable, map } from 'rxjs'; //-used if async pipe subscription is being implemented
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
 export class BooksComponent implements OnInit {
-  books!: Book[] | any[];
-  filteredBooks!: Book[];
+  books!: Book[] | any[]; //-  books$!: Observable<Book[] | any[]> is when async pipe is being used
+  filteredBooks!: Book[]; //- filteredBooks$!: Observable<Book[]> is when async pipe is being used
   @ViewChild('inputElement', { static: false })
   inputElement!: ElementRef;
   constructor(private service: BooksService) { }
@@ -21,11 +18,13 @@ export class BooksComponent implements OnInit {
     this.getBooks();
   }
 
-  getBooks(): any {
+  getBooks(): void {
     this.service.getBooks().subscribe(data => {
       this.books = data;
       this.filteredBooks = this.books;
     })
+    // this.books$ = this.service.getBooks();
+    // this.filteredBooks$ = this.books$;
   }
 
   filterBooks() {
@@ -33,9 +32,18 @@ export class BooksComponent implements OnInit {
     this.filteredBooks = this.books.filter((obj) => obj.author.toLowerCase().includes(val)
       || obj.title.toLowerCase().includes(val));
     return this.filteredBooks;
+    //this.filteredBooks$ = this.books$.pipe(map(books => books.filter((obj) => obj.author.toLowerCase().includes(val) || obj.title.toLowerCase().includes(val))));
   }
 
   deleteBook(id: number) {
     this.service.deleteBook(id).subscribe(() => this.getBooks());
   }
+
+  // deleteBook(event: Event, id: number) {
+  //   event.preventDefault();
+  //   this.service.deleteBook(id).subscribe(() => {
+  //     this.books$ = this.service.getBooks();
+  //     this.filteredBooks$ = this.books$;
+  //   }); //- is when async pipe is being used
+  // }
 }
